1d09717cd4f0baad382193b04250c632e06f4a22
TagaibekEdil <Tagaibek.edil@gmail.com>
2023-03-26 21:14:53 +0600

    first

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Main {
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> integers = new ArrayList<>(20);//1
+        for (int i = 0; i <= 20; i++) {
+            integers.add(RandomUtil.getRandomNumber());
+        }
+        Set<Integer> unUnikum = new HashSet<>();//2
+        List<Integer> unikum = new ArrayList<>();
+
+        for (Integer inter : integers) {
+            if (!unUnikum.add(inter)) {
+                unikum.add(inter);
+            }
+        }
+        System.out.println("Не повторяющиеся числа: " + unUnikum);
+        System.out.println("Повторяющиеся числа: " + unikum);
+    }
 }
--- src/RandomUtil.java
+++ src/RandomUtil.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class RandomUtil {
+import java.util.Random;
+public class RandomUtil {
+
+    Random random = new Random();
+    public static int getRandomNumber() {
+        return (int) (Math.random() * (9 - 1)) + 1;
+    }
 }
+
